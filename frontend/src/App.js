ad026678f033855781a483aeed17cb457ef57b9b
Bryan King Pecho <bkpecho05@gmail.com>
2023-07-27 00:29:05 +0800

    feat(App): Integrate `import { Outlet } from 'react-router-dom';` into App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,7 @@
 import { Container } from 'react-bootstrap';
+import { Outlet } from 'react-router-dom';
 import Footer from './components/Footer';
 import Header from './components/Header';
-import HomeScreen from './screens/HomeScreen';
 
 const App = () => {
   return (
@@ -9,7 +9,7 @@ const App = () => {
       <Header />
       <main classname="py-3">
         <Container>
-          <HomeScreen />
+          <Outlet />
         </Container>
       </main>
       <Footer />
