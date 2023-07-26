4a244a102a2996d95110abaa72cabbca9f203b45
Bryan King Pecho <bkpecho05@gmail.com>
2023-07-26 22:18:18 +0800

    feat(Index): Integrate custom Bootstrap and styling into index.js

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import 'bootstrap/dist/css/bootstrap.min.css';
+// import 'bootstrap/dist/css/bootstrap.min.css';
 import App from './App';
-import './index.css';
+import './assets/styles/bootstrap.custom.css';
+import './assets/styles/index.css';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
