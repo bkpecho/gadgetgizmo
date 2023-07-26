b318f0e7f8db3ccc46abb71a17f9f39880899984
Bryan King Pecho <bkpecho05@gmail.com>
2023-07-26 21:59:54 +0800

    feat(App): Include Header and Footer components in App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,19 @@
-import React from 'react';
+import { Container } from 'react-bootstrap';
+import Footer from './components/Footer';
+import Header from './components/Header';
 
 const App = () => {
-  return <h1>Welcome to PinoyTechShop</h1>;
+  return (
+    <>
+      <Header />
+      <main classname="py-3">
+        <Container>
+          <h1>Welcome to PinoyTechShop</h1>
+        </Container>
+      </main>
+      <Footer />
+    </>
+  );
 };
 
 export default App;
