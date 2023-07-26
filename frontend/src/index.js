7fe134f3d8b100d0e6d086368c92eddfeab788bd
Bryan King Pecho <bkpecho05@gmail.com>
2023-07-27 00:27:36 +0800

    feat(Frontend): Implement React Router for navigation in index.js

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,15 +1,30 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+  RouterProvider
+} from 'react-router-dom';
 // import 'bootstrap/dist/css/bootstrap.min.css';
 import App from './App';
 import './assets/styles/bootstrap.custom.css';
 import './assets/styles/index.css';
 import reportWebVitals from './reportWebVitals';
+import HomeScreen from './screens/HomeScreen';
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<App />}>
+      <Route index={true} path="/" element={<HomeScreen />} />
+    </Route>
+  )
+);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
 
