458541f4269788ca55ddbd120e45199a6408e756
Bryan King Pecho <bkpecho05@gmail.com>
2023-07-27 20:35:11 +0800

    feat(Backend): Updated server.js to use productRoutes for handling product-related routes

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,6 @@
 import express from 'express';
 import connectDB from './config/db.js';
-import products from './data/products.js';
+import productRoutes from './routes/productRoutes.js';
 
 import dotenv from 'dotenv';
 dotenv.config();
@@ -15,14 +15,7 @@ app.get('/', (req, res) => {
   res.send('💚 API is running!');
 });
 
-app.get('/api/products', (req, res) => {
-  res.json(products);
-});
-
-app.get('/api/products/:id', (req, res) => {
-  const product = products.find((p) => p._id === req.params.id);
-  res.json(product);
-});
+app.use('/api/products', productRoutes);
 
 app.listen(port, () => {
   console.log(`🟢 Server running on port ${port}`);
