b6b27a6bbc10993950f0a62b9a2dc072725b0808
Bryan King Pecho <bkpecho05@gmail.com>
2023-07-27 21:00:19 +0800

    feat(Backend): Include the errorMiddleware for handling 404 errors (notFound) and general error handling (errorHandler)

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,9 @@
+import dotenv from 'dotenv';
 import express from 'express';
 import connectDB from './config/db.js';
-import productRoutes from './routes/productRoutes.js';
+import { errorHandler, notFound } from './middleware/errorMiddleware.js';
 
-import dotenv from 'dotenv';
+import productRoutes from './routes/productRoutes.js';
 dotenv.config();
 
 const port = process.env.PORT || 5000;
@@ -16,6 +17,9 @@ app.get('/', (req, res) => {
 });
 
 app.use('/api/products', productRoutes);
+
+app.use(notFound);
+app.use(errorHandler);
 
 app.listen(port, () => {
   console.log(`🟢 Server running on port ${port}`);
