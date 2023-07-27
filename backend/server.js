85c43bb53955e76766da3abf24cbcba9d03871ed
Bryan King Pecho <bkpecho05@gmail.com>
2023-07-27 16:44:07 +0800

    chore(Backend): Refactored server.js to connect to the MongoDB database using connectDB() function from db.js

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,13 @@
 import express from 'express';
+import connectDB from './config/db.js';
 import products from './data/products.js';
 
 import dotenv from 'dotenv';
 dotenv.config();
 
 const port = process.env.PORT || 5000;
+
+connectDB();
 
 const app = express();
 
