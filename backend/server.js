cfd31f662e14aacd664ef5bde668439485a25d8d
Bryan King Pecho <bkpecho05@gmail.com>
2023-07-31 19:47:47 +0800

    chore(server): Add body parser middleware

--- backend/server.js
+++ backend/server.js
@@ -13,6 +13,11 @@ connectDB();
 
 const app = express();
 
+// Body parser middleware
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
 app.get('/', (req, res) => {
   res.send('💚 API is running!');
 });
