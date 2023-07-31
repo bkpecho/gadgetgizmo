f1065cdeb4df1d414a041a21b3af69ce6cf30387
Bryan King Pecho <bkpecho05@gmail.com>
2023-07-31 20:29:13 +0800

    feat(userRoutes): Implement authentication middleware in routes

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -13,10 +13,19 @@ import {
   updateUserProfile
 } from '../controllers/userController.js';
 
-router.route('/').post(registerUser).get(getUsers);
+import { admin, protect } from '../middleware/authMiddleware.js';
+
+router.route('/').post(registerUser).get(protect, admin, getUsers);
 router.post('/logout', logoutUser);
 router.post('/login', loginUser);
-router.route('/profile').get(getUserProfile).put(updateUserProfile);
-router.route('/:id').delete(deleteUser).get(getUsersByID).put(updateUser);
+router
+  .route('/profile')
+  .get(protect, getUserProfile)
+  .put(protect, updateUserProfile);
+router
+  .route('/:id')
+  .delete(protect, admin, deleteUser)
+  .get(protect, admin, getUsersByID)
+  .put(protect, admin, updateUser);
 
 export default router;
