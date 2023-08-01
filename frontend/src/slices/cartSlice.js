2f1dee9c9a3fdd34bef78166ad9a2351a82b3648
Bryan King Pecho <bkpecho05@gmail.com>
2023-08-01 19:16:52 +0800

    feat(CartSlice): Add clearCartItems functionality to cartSlice

--- frontend/src/slices/cartSlice.js
+++ frontend/src/slices/cartSlice.js
@@ -36,6 +36,10 @@ const cartSlice = createSlice({
     savePaymentMethod: (state, action) => {
       state.paymentMethod = action.payload;
       return updateCart(state);
+    },
+    clearCartItems: (state, action) => {
+      state.cartItems = [];
+      return updateCart(state);
     }
   }
 });
@@ -44,7 +48,8 @@ export const {
   addToCart,
   removeFromCart,
   saveShippingAddress,
-  savePaymentMethod
+  savePaymentMethod,
+  clearCartItems
 } = cartSlice.actions;
 
 export default cartSlice.reducer;
