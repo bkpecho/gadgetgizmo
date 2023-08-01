6235a34d4f4c0a6d2a207d5cf67509a3385b5083
Bryan King Pecho <bkpecho05@gmail.com>
2023-08-01 11:32:55 +0800

    feat(PaymentScreen): Create PaymentScreen component for selecting the payment method during checkout

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -17,6 +17,7 @@ import reportWebVitals from './reportWebVitals';
 import CartScreen from './screens/CartScreen';
 import HomeScreen from './screens/HomeScreen';
 import LoginScreen from './screens/LoginScreen';
+import PaymentScreen from './screens/PaymentScreen';
 import ProductScreen from './screens/ProductScreen';
 import RegisterScreen from './screens/RegisterScreen';
 import ShippingScreen from './screens/ShippingScreen';
@@ -32,6 +33,7 @@ const router = createBrowserRouter(
 
       <Route path="" element={<PrivateRoute />}>
         <Route path="/shipping" element={<ShippingScreen />} />
+        <Route path="/payment" element={<PaymentScreen />} />
       </Route>
     </Route>
   )
