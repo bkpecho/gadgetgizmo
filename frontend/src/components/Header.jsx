d82eec222637909e3f27f74b565e3b73b163f374
Bryan King Pecho <bkpecho05@gmail.com>
2023-08-06 13:49:26 +0800

    fix(Header): Navbar expansion issue for md to lg

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -30,7 +30,7 @@ const Header = () => {
 
   return (
     <header>
-      <Navbar bg="dark" variant="dark" expand="md" collapseOnSelect>
+      <Navbar bg="dark" variant="dark" expand="lg" collapseOnSelect>
         <Container>
           <LinkContainer to="/">
             <Navbar.Brand className="d-flex gap-2 align-items-center">
