3c578bbf4b28cf5ac064c4898d7571053808b46f
Bryan King Pecho <bkpecho05@gmail.com>
2023-08-08 23:29:13 +0800

    chore(Server): Added static file serving for frontend dist directory

--- backend/server.js
+++ backend/server.js
@@ -34,8 +34,7 @@ app.get('/api/config/paypal', (req, res) =>
 // Connect to MongoDB before starting the server
 connectDB().then(() => {
   if (process.env.NODE_ENV === 'production') {
-    app.use(express.static(process.env.CLOUDINARY_URL));
-
+    app.use(express.static(path.join(__dirname, '/frontend/dist')));
     const __dirname = path.resolve();
     app.get('*', (req, res) =>
       res.sendFile(path.resolve(__dirname, 'frontend', 'dist', 'index.html'))
