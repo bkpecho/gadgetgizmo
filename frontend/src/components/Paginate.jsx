dae5fa0f93c9b74e01829b61a915b4bb530cf481
Bryan King Pecho <bkpecho05@gmail.com>
2023-08-08 10:50:24 +0800

    chore(Paginate): Remove unused variable

--- frontend/src/components/Paginate.jsx
+++ frontend/src/components/Paginate.jsx
@@ -1,8 +1,6 @@
 import { Pagination } from 'react-bootstrap';
 import { LinkContainer } from 'react-router-bootstrap';
 
-import React from 'react';
-
 const Paginate = ({ pages, page, isAdmin = false, keyword = '' }) => {
   return (
     pages > 1 && (
